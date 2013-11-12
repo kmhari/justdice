9471202f93bd0d23f4ba80737b9a3790ce676e5b
hariharasudhan <hariharasudhan@live.com>
2013-11-12 22:19:18 +0530

    Initial repo which receives the message

---
+++ app.js
@@ -0,0 +1,57 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
+var mysql      = require('mysql');
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 8000);
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+});
+
+// connection.connect();
+
+var server = http.createServer(app);
+var io = socketio.listen(server);
+
+
+io.sockets.on('connection', function(socket){
+    socket.on('message', function(message){
+        console.log('received message:', message);
+        // io.sockets.emit('message', message);
+    });
+});
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+function test()
+{connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+}
+connection.end();
