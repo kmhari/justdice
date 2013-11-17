bdf3d4c8833eb099240a6f8b20bf1f1bb9c1945c
hariharasudhan <hariharasudhan@live.com>
2013-11-17 22:02:07 +0530

    Added error codes

--- error_code.js
+++ error_code.js
@@ -1,4 +1,15 @@
 var err_code = {
-    1: "Gid Not Found, Cookie Deleted"
-}
+    1: {
+        error: 1,
+        message: "Kindly delete your cookies and try again"
+    },
+    2: {
+        error:2,
+        message: "Insufficient balance"
+    },
+    3: {
+        error:3,
+        message: "Username not available`"
+    }
+};
 module.exports = err_code;
